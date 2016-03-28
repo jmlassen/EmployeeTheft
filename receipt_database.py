b6df0f5dea618f070e5e7cf291d10d3cad0e84e7
Joel Lassen <jmlassen@gmail.com>
2016-03-28 14:17:06 -0600

    Stubbed code to read receipts from file and create a Receipt object for each one.

--- receipt_database.py
+++ receipt_database.py
@@ -1,4 +1,7 @@
+import os
 from os import listdir
+
+from receipt import Receipt
 
 RECEIPT_DIRECTORY = "data/"
 
@@ -9,7 +12,26 @@ class ReceiptDatabase:
         """
         receipts = []
         receipt_filenames = listdir(RECEIPT_DIRECTORY)
+        for filename in receipt_filenames:
+            receipt_filename = "{}{}".format(RECEIPT_DIRECTORY, filename)
+            receipt_lines = self._get_receipt_lines(receipt_filename)
+            receipt = self._objectify_receipt_from_lines(receipt_lines)
+        return receipts
+
+    def _get_receipt_lines(self, receipt_filename):
+        """Reads a receipt file into an array of strings.
+        """
+        lines = []
+        with open(receipt_filename) as file:
+            for line in file:
+                lines.append(line)
+        return lines
+
+    def _objectify_receipt_from_lines(self, receipt_lines):
+        """Transforms receipt lines into an object.
+        """
+        return Receipt()
 
 
 if __name__ == '__main__':
-    ReceiptDatabase().load_receipts()
+    receipts = ReceiptDatabase().load_receipts()
