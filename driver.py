27635dfd54bdb95d608408f412e604b41a4925d0
Grant Merrill <grant.merril@live.com>
2016-04-01 16:03:59 -0600

    Modified todo list. Added classifiers to Driver.

--- driver.py
+++ driver.py
@@ -1,9 +1,16 @@
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+
 class Driver:
     def __init__(self):
         pass
 
     def run_accuracy_test(self):
         # Create a list of classifiers
+        nb = GaussianNB()
+        svm = SVC()
+        knc = KNeighborsClassifier()
 
         # Create a list of classifier preprocessor functions
 
